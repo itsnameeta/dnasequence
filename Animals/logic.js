28b44de967e41ea1024373e6049341271c4f133f
Zachary Breen <zgbreen@gmail.com>
2021-12-08 00:57:50 +1100

    Create logic.js

---
+++ Animals/logic.js
@@ -0,0 +1,43 @@
+// Create a map object
+var myMap = L.map("map", {
+  center: [37, -95],
+  zoom: 3.5
+});
+
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/streets-v11",
+  accessToken: API_KEY
+}).addTo(myMap);
+
+// Country data
+var animals = [
+  {
+    name: "Rocky Mountain National Park, Colorado",
+    location: [40.343182, -105.688103]
+  },
+  {
+    name: "Katmai National Park and Preserve, King Salmon",
+    location: [58.597813,	-154.693756]
+  },
+  {
+    name: "Glacier Bay National Park and Preserve, Alaska",
+    location: [58.665806,	-136.900208]
+  },
+  {
+    name: "Channel Islands National Park, Ventura, CA",
+    location: [33.998028,	-119.77294]
+  },
+];
+
+// Loop through the cities array and create one marker for each city, bind a popup containing its name and population add it to the map
+for (var i = 0; i < animals.length; i++) {
+  var animals = animals[i];
+  L.marker(animals.location)
+    .bindPopup("<h1>" + animals.name + "</h1>" )
+    .addTo(myMap);
+}
+
